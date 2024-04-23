f06501da5292d0425aa3c4c236aeac046e932af3
Johnny <johdu13@hotmail.fr>
2024-04-23 07:25:32 +0200

    First junit4 test getKeywords

--- app/src/test/java/com/example/gamecarx/CardTest.kt
+++ app/src/test/java/com/example/gamecarx/CardTest.kt
@@ -1,15 +1,28 @@
 package com.example.gamecarx
 
 import org.junit.Assert.*
+import org.junit.Before
 
 import org.junit.Test
+import org.junit.runners.Parameterized
 
 class CardTest {
+    private lateinit var cards: Cards
+    private lateinit var card1: Card
+    private lateinit var card2: Card
+    @Before
+    fun setUp() {
+        cards = Cards("C:\\Users\\johdu\\AndroidStudioProjects\\GameCarx\\app\\src\\main\\res\\dataset\\cards_base.csv")
+        card1 = cards.getCards()?.get(1)!! // first card
+        card2 = cards.getCards()?.get(2)!! // second card
+    }
 
     @Test
     fun getKeywords() {
-        // TODO
-        assertEquals(1,1)
+        var kwc1: List<String> = listOf("Poisonous")
+        assertEquals(/* expected = */ kwc1, /* actual = */ card1.keywords)
+        var kwc2: List<String> = "".split(",")
+        assertEquals(/* expected = */ kwc2, /* actual = */ card2.keywords)
     }
 
     @Test
