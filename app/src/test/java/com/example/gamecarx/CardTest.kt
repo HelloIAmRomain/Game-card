8c338a81e122c3a167a29acf47c6bd113ca0c8e6
Johnny <johdu13@hotmail.fr>
2024-04-30 07:42:52 +0200

    Junit4 cardTest

--- app/src/test/java/com/example/gamecarx/CardTest.kt
+++ app/src/test/java/com/example/gamecarx/CardTest.kt
@@ -27,17 +27,33 @@ class CardTest {
 
     @Test
     fun getId_card() {
+        var kwc1: String = "1"
+        assertEquals(/* expected = */ kwc1, /* actual = */ card1.id_card)
+        var kwc2: String = "2"
+        assertEquals(/* expected = */ kwc2, /* actual = */ card2.id_card)
     }
 
     @Test
     fun getName() {
+        var kwc1: String = "Axolotl Healer"
+        assertEquals(/* expected = */ kwc1, /* actual = */ card1.name)
+        var kwc2: String = "Bee Bear"
+        assertEquals(/* expected = */ kwc2, /* actual = */ card2.name)
     }
 
     @Test
     fun getPower() {
+        var kwc1: String = "4"
+        assertEquals(/* expected = */ kwc1, /* actual = */ card1.power)
+        var kwc2: String = "8"
+        assertEquals(/* expected = */ kwc2, /* actual = */ card2.power)
     }
 
     @Test
     fun getDescription() {
+        var kwc1: String = "Play: Gain 2 life points"
+        assertEquals(/* expected = */ kwc1, /* actual = */ card1.description)
+        var kwc2: String = "Cannot be blocked by creatures with 6 power or less"
+        assertEquals(/* expected = */ kwc2, /* actual = */ card2.description)
     }
 }
