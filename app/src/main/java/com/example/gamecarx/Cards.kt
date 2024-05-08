1ddca08ddd62b5cb6d8be800a71fd3bdb7635019
Johnny <johdu13@hotmail.fr>
2024-05-08 23:08:08 +0200

    shuffleCards and swapCards corrected

--- app/src/main/java/com/example/gamecarx/Cards.kt
+++ app/src/main/java/com/example/gamecarx/Cards.kt
@@ -12,7 +12,7 @@ class Cards {
         this.cardsPath = cardsPath
     }
 
-    fun getCards(): List<Card>? {
+    fun getCards(): MutableList<Card>? {
         val cards = mutableListOf<Card>()
         var nb: Int
         try {
@@ -27,7 +27,7 @@ class Cards {
                 val fields = line!!.split("|")
                 // Process the fields as needed
                 nb = fields[5].toInt()
-                println(fields[0])
+                //println(fields[0])
                 // Add one or more cards
                 while(nb > 0) {
                     cards.add(Card(fields[0], fields[1], fields[2], fields[3], fields[4]))
@@ -41,25 +41,26 @@ class Cards {
         }
         return null
     }
-    fun swapCards(cards: MutableList<Card>, i: Int, j: Int): MutableList<Card> {
+    fun swapCards(cards: MutableList<Card>?, i: Int, j: Int): MutableList<Card> {
         var jbis = j
         if(i == j) { // Case it is the same card, take the next card
             jbis = j + 1
         }
-        var t = cards[i]
+        var t = cards!![i]
         cards[i] = cards[jbis]
-        cards[j] = t
+        cards[jbis] = t
         return cards
     }
 
-    fun shuffleCards(cards: List<Card>, i_MIN: Int=200, I_MAX: Int=500): List<Card> {
+    fun shuffleCards(cards: MutableList<Card>, i_MIN: Int=200, i_MAX: Int=500): List<Card> {
         // shuffle the cards given
         var rnd_swaps = (i_MIN..i_MAX).random()
         var i_swap = (0..cards.size-2).random()
         var j_swap = (0..cards.size-2).random()
+        var cards_ = cards
         while(rnd_swaps>0) { // for each swap
             --rnd_swaps
-            cards = swapCards(cards, i_swap, j_swap)
+            cards_ = swapCards(cards_, i_swap, j_swap)
             i_swap = (0..cards.size-2).random() // take number between 0 and max numbers cards less 2
             j_swap = (0..cards.size-2).random() // take number between 0 and max numbers cards less 2
             //println(i_swap.toString() + " " + j_swap + " " + cards)
